64b9c0d36461effcbe04e222aa5bdc15d0652c6d
ukelly <ukelly@student.21-school.ru>
2020-12-19 21:42:17 +0300

    Добавила глобальные переменные, тип (имя,коммент и т.д.), начала парсер, с файла asm начинается работа ассемблера

--- include/asm.h
+++ include/asm.h
@@ -16,7 +16,11 @@
 # include "op.h"
 # include "op_struct.h"
 
-# define ALT_COMMENT_CHAR		';'
+# define ALT_COMMENT_CHAR	';'
+
+char 	*name;
+char 	*comment;
+int		code_size;
 
 typedef enum			e_line_type
 {
@@ -26,21 +30,10 @@ typedef enum			e_line_type
 	LINE_ARGUMENT
 }						t_line_type;
 
-typedef struct			s_parser
-{
-	int					fd;
-	unsigned 			line;
-	char				*name;
-	char				*comment;
-	char				*code;
-}						t_parser;
-
-char	*ft_strtrim(char const *s);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_strrev(register char *begin);
-void	*assembler(char *filename);
-void	*disassembler(char *filename);
-int 	get_line(int fd, char *line);
-void	parse(int fd);
+void					*ft_strrev(register char *begin);
+void					*assembler(char *filename);
+void					*disassembler(char *filename);
+int						get_line(int fd, char *line);
+void					parse(int fd);
 
 #endif
---
+++ source/assembler/asm.c
@@ -0,0 +1,16 @@
+#include "asm.h"
+
+void	*assembler(char *filename)
+{
+	int		fd;
+
+	if ((fd = open(filename, O_RDONLY)) == -1)
+		;//error(ERR_OPEN_FILE)
+	parse(fd);
+//	filename = replace_extension(filename, ".s", ".cor");
+	if ((fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0644)) == -1)
+		;//error(ERR_CREATE_FILE);
+	//bite code record in file
+	ft_putstr("Writing output program to ");
+	ft_putstr(filename);
+}
