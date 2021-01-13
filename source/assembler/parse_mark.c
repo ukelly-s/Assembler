779ede2a101f43076c34d8589807ca72e365adf6
ukelly <ukelly@student.21-school.ru>
2021-01-13 05:33:34 +0300

    редактирование parse_mark

--- source/assembler/parse_mark.c
+++ source/assembler/parse_mark.c
@@ -10,5 +10,4 @@ void				parse_mark(char *str, t_hashmap *info_mark, t_parse *g)
 		return;
 	ft_putstr(str);//fixme delete
 	hashmap_put(info_mark, str, ft_strlen(str) - 1, (void*)g->header->prog_size);//todo у Вовы спросить
-	g->header->prog_size++;
 }
