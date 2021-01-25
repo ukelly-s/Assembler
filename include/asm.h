fcf70441dd7d3bf09b063eee6151bde6f36515ab
ukelly <ukelly@student.21-school.ru>
2021-01-25 23:43:42 +0300

    finish norminette

--- include/asm.h
+++ include/asm.h
@@ -18,9 +18,9 @@
 # define ALT_COMMENT_CHAR	';'
 # define REGISTER_CHAR		'r'
 
-# define ARG_REG 0b01000000
-# define ARG_DIR 0b10000000
-# define ARG_IND 0b11000000
+# define ARG_REG			0b01000000
+# define ARG_DIR			0b10000000
+# define ARG_IND			0b11000000
 
 # include "list.h"
 # include "op.h"
@@ -32,14 +32,14 @@
 ** lexer
 */
 
-typedef enum		e_name_comment_flags
+typedef enum	e_name_comment_flags
 {
 	FLAG_DEFAULT,
 	FLAG_NAME,
 	FLAG_COMMENT
-}					t_name_comment_flags;
+}				t_name_comment_flags;
 
-typedef enum		e_line_type
+typedef enum	e_line_type
 {
 	LINE_UNDEFINED,
 	LINE_EMPTY,
@@ -47,42 +47,42 @@ typedef enum		e_line_type
 	LINE_COMMENT,
 	LINE_OPERATION,
 	LINE_MARK
-}					t_line_type;
+}				t_line_type;
 
 typedef struct	s_cmd
 {
 	uint8_t		code;
 	uint8_t		args_types[3];
-	int32_t 	args_value[3];
-	char 		*mark[3];
+	int32_t		args_value[3];
+	char		*mark[3];
 	uint32_t	size_op;
 }				t_cmd;
 
-typedef union	s_code4b
+typedef union	u_code4b
 {
 	int32_t		digit;
-	uint8_t 	letter[4];
+	uint8_t		letter[4];
 }				t_code4b;
 
-typedef union	s_code2b
+typedef union	u_code2b
 {
 	int16_t		digit;
 	uint8_t		letter[2];
 }				t_code2b;
 
-typedef struct		s_parse
+typedef struct	s_parse
 {
-	int				name;
-	int				comment;
-	t_header		*header;
-	t_byte			*byte_str;
-}					t_parse;
+	int			name;
+	int			comment;
+	t_header	*header;
+	t_byte		*byte_str;
+}				t_parse;
 
 /*
 ** main.c
 */
 
-_Bool	check_name_the_file(char *name_the_file, char *file_extension);
+_Bool			check_name_the_file(char *name_the_file, char *file_extension);
 
 /*
 ** asm.c
@@ -101,10 +101,10 @@ void			get_to_2byte(t_byte *str, int32_t args_value);
 ** parse.c
 */
 
-int					get_number_operation(const char *str);
-int					get_line(int fd, char **line);
-void				parse(int fd, t_parse *g, t_list *info_operations,
-						  t_hashmap *info_mark);
+int				get_number_operation(const char *str);
+int				get_line(int fd, char **line);
+void			parse(int fd, t_parse *g, t_list *info_operations,
+						t_hashmap *info_mark);
 
 /*
 ** parse_clear_line.c
@@ -115,13 +115,13 @@ char			*clear_line(char **str);
 /*
 ** libft
 */
-char				*ft_strtrim(char const *s);
+char			*ft_strtrim(char const *s);
 
 /*
 ** parse_mark.c
- */
+*/
 
-void	parse_mark(char *str, t_hashmap *info_mark, t_parse *g);
+void			parse_mark(char *str, t_hashmap *info_mark, t_parse *g);
 
 /*
 ** parse_name_comment.c
@@ -140,7 +140,7 @@ void			parse_operation(char *str, t_list *all_op, t_parse *g);
 ** translation_bytecode
 */
 void			translation_bytecode(t_list *operations,
-									 t_hashmap *mark, t_parse *g);
+										t_hashmap *mark, t_parse *g);
 
 /*
 ** util.c
@@ -149,7 +149,7 @@ void			translation_bytecode(t_list *operations,
 t_line_type		mark_operation_type(const char *str);
 void			get_prog_size(t_cmd *cmd);
 char			*replace_extension(char *filename, char *file_extension_asm,
-								   char *file_extension_disasm);
+									char *file_extension_disasm);
 void			free_cmd(void *data);
 
 #endif
